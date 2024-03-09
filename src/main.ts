1cc1886f26df0915f87baf939e7e4a8638f014ea
Hamid <halavatoje@gmail.com>
2024-03-09 21:38:18 +0500

    last

--- src/main.ts
+++ src/main.ts
@@ -6,5 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const TgBotService = app.get(AppBot);
   await TgBotService.startBot();
+  app.enableCors()
+  await app.listen(process.env.PORT || 3000)
 }
 bootstrap();
